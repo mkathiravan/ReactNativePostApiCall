14e5e77a58476eb482278d9e0a4a02c097dd213b
Kathiravan <mkathiravanmca@gmail.com>
2021-06-16 10:04:22 +0530

    Update the app.js file

--- App.js
+++ App.js
@@ -2,13 +2,43 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  async componentDidMount()
+  {
+    try{
+
+      await fetch('https://webhook.site/4a1dfbea-8c7c-41fd-9580-1b7618f5eada',{
+        method: 'post',
+        mode: 'no-cors',
+        headers:{
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+
+          username: 'kathiravan',
+          password: 'zizu'
+
+        })
+
+      })
+    }catch(e)
+    {
+      console.log(e);
+    }
+  }
+
+  render()
+  {
+    return (
+      <View style={styles.container}>
+        <Text>Post data Call</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
